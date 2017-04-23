923be6ccce0c95207eb36ced5fcc2ca9665509fa
xlitter <9638527_4_1@163.com>
2017-04-23 12:47:33 +0800

    first init

---
+++ weex-zhihu/src/foo.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrapper" @click="update">
+    <image :src="logoUrl" class="logo"></image>
+    <text class="title">Hello {{target}}</text>
+    <text v-for="item in list">{{item.name}}</text>
+  </div>
+</template>
+
+<style>
+  .wrapper {
+    align-items: center;
+    margin-top: 120px;
+  }
+
+  .title {
+    font-size: 48px;
+  }
+
+  .logo {
+    width: 360px;
+    height: 82px;
+  }
+</style>
+
+<script>
+  export default {
+    data: {
+      logoUrl: 'https://alibaba.github.io/weex/img/weex_logo_blue@3x.png',
+      target: 'World',
+      flag: true,
+      list: Array.apply(null, {
+        length: 5
+      }).map((v, i) => ({
+        name: `name${i}`,
+      }))
+    },
+    methods: {
+      update: function (e) {
+        if (this.flag) {
+          this.target = 'Weex';
+          this.flag = false;
+        } else {
+          this.target = 'World'
+          this.flag = true;
+        }
+
+        console.log('target:', this.target)
+      }
+    }
+  }
+</script>
